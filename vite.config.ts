e075c039516207d29db9d6f762d7f507cb74133c
Martin Aguilar <89709211+Marttify@users.noreply.github.com>
2025-12-06 14:29:29 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/list_items_react/',
   plugins: [react()],
   test: {
     environment: 'happy-dom'
